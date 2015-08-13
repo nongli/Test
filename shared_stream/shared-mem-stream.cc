7b8c794a6793879e1e83d4342a55283a3087b082
Nong Li <nong@cloudera.com>
2015-08-13 00:36:22 -0700

    Updates to shared mem.

--- shared_stream/shared-mem-stream.cc
+++ shared_stream/shared-mem-stream.cc
@@ -9,8 +9,8 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-
-SharedMemoryStream* SharedMemoryStream::Create(const char* path, int64_t capacity) {
+SharedMemoryStream* SharedMemoryStream::Create(const char* path, int64_t capacity,
+    int max_messages) {
   int fd = shm_open(path, O_CREAT | O_RDWR, 0666);
   if (fd < 0) {
     printf("Could not open: %s\n", path);
@@ -23,17 +23,23 @@ SharedMemoryStream* SharedMemoryStream::Create(const char* path, int64_t capacit
   stream->capacity_ = capacity;
   stream->fd_ = fd;
   stream->is_writer_ = true;
-  stream->memory_ = (char*)mmap(0, capacity, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  if (stream->memory_ == MAP_FAILED) {
+  char* memory = (char*)mmap(0, capacity, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (memory == MAP_FAILED) {
     printf("Could not mmap.\n");
     stream->Close();
     return NULL;
   }
+
+  stream->control_ = (Control*)memory;
+  memset(stream->control_, 0, sizeof(Control));
+  stream->control_->max_messages = max_messages;
+  stream->memory_ = memory + sizeof(Control) + max_messages * sizeof(Message);
+  stream->write_head_ = stream->memory_;
   return stream;
 }
 
 SharedMemoryStream* SharedMemoryStream::Open(const char* path) {
-  int fd = shm_open(path, O_RDONLY, 0666);
+  int fd = shm_open(path, O_RDWR, 0666);
   if (fd < 0) {
     printf("Could not open: %s\n", path);
     return NULL;
@@ -49,24 +55,64 @@ SharedMemoryStream* SharedMemoryStream::Open(const char* path) {
   stream->capacity_ = file_stat.st_size;
   stream->fd_ = fd;
   stream->is_writer_ = false;
-  stream->memory_ = (char*)mmap(0, stream->capacity_, PROT_READ , MAP_SHARED, fd, 0);
-  if (stream->memory_ == MAP_FAILED) {
+  char* memory =
+      (char*)mmap(0, stream->capacity_, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (memory == MAP_FAILED) {
     printf("Could not mmap.\n");
     stream->Close();
     return NULL;
   }
+  stream->control_ = (Control*)memory;
+  stream->memory_ =
+      memory + sizeof(Control) + stream->control_->max_messages * sizeof(Message);
   return stream;
 }
 
 char* SharedMemoryStream::GetBufferForWrite(int64_t len) {
   if (!is_writer_) return NULL;
   if (len > capacity_) return NULL;
-  return memory_;
+  while (true) {
+    int bytes_used = write_head_ - memory_;
+    if (capacity_ - bytes_used >= len) break;
+    // wait. When we get signaled by the reader, we can wrap around and
+    // reset write_head_ to memory_
+  }
+  return write_head_;
 }
 
-char* SharedMemoryStream::Read(int64_t* len) {
+bool SharedMemoryStream::CommitMessage(int64_t len) {
+  while (true) {
+    uint64_t num_messages = control_->write_idx - control_->read_idx;
+    if (num_messages != control_->max_messages) break;
+    // wait
+  }
+  int message_idx = control_->write_idx % control_->max_messages;
+  control_->messages[message_idx].offset = write_head_ - memory_;
+  control_->messages[message_idx].len = len;
+  ++control_->write_idx;
+  // Signal
+  write_head_ += len;
+  return true;
+}
+
+char* SharedMemoryStream::ReadMessage(int64_t* len) {
   if (is_writer_) return NULL;
-  return memory_;
+
+  while (true) {
+    uint64_t num_messages = control_->write_idx - control_->read_idx;
+    if (num_messages != 0) break;
+    // wait
+  }
+
+  int message_idx = control_->read_idx % control_->max_messages;
+  *len = control_->messages[message_idx].len;
+  return memory_ + control_->messages[message_idx].offset;
+}
+
+void SharedMemoryStream::ReadDone() {
+  int message_idx = control_->read_idx % control_->max_messages;
+  ++control_->read_idx;
+  // Signal
 }
 
 void SharedMemoryStream::Close() {
@@ -77,9 +123,10 @@ void SharedMemoryStream::Close() {
 
 int main(int argc, char** argv) {
   const int64_t CAPACITY = 4096;
+  const int MAX_MESSAGES = 10;
   const char* PATH = "/test";
 
-  SharedMemoryStream* writer = SharedMemoryStream::Create(PATH, CAPACITY);
+  SharedMemoryStream* writer = SharedMemoryStream::Create(PATH, CAPACITY, MAX_MESSAGES);
   if (writer == NULL) {
     printf("Couldn't create writer.\n");
     return -1;
@@ -93,12 +140,13 @@ int main(int argc, char** argv) {
 
   const char* MSG = "this works.";
   char* write_buffer = writer->GetBufferForWrite(1024);
-  sprintf(write_buffer, "%s", MSG);
-
+  int bytes_written = sprintf(write_buffer, "%s", MSG);
+  writer->CommitMessage(bytes_written);
 
   int64_t read_len;
-  char* read_buffer = reader->Read(&read_len);
-  printf("READ: %s\n", read_buffer);
+  char* read_buffer = reader->ReadMessage(&read_len);
+  printf("READ: %s(%ld)\n", read_buffer, read_len);
+  reader->ReadDone();
 
   writer->Close();
   reader->Close();
--- shared_stream/shared-mem-stream.h
+++ shared_stream/shared-mem-stream.h
@@ -4,10 +4,13 @@
 #include <boost/cstdint.hpp>
 #include <string>
 
-// Cross proc single reader, single writer stream.
+// Cross proc single reader, single writer stream backed using shared memory.
+// Designed for large bulk message-based transfers with zero copies.
+// We can use cross proc semaphores to signal or rely on some other IPC mechanism.
 class SharedMemoryStream {
  public:
-  static SharedMemoryStream* Create(const char* path, int64_t capacity);
+  static SharedMemoryStream* Create(
+      const char* path, int64_t capacity_bytes, int max_messages);
   static SharedMemoryStream* Open(const char* path);
 
   void Close();
@@ -16,30 +19,57 @@ class SharedMemoryStream {
   // size is too big (larger than capacity). Blocks if the there is not enough bytes
   // available.
   char* GetBufferForWrite(int64_t len);
-  // Commits len written bytes available to be read. The writer cannot modify those
+  // Commits a message of len bytes available to be read. The writer cannot modify those
   // bytes until another call to GetBufferForWrite().
-  void CommitWrite(int64_t len);
+  bool CommitMessage(int64_t len);
 
   // Returns the next available bytes to be read, blocking until more bytes are
   // available.
-  char* Read(int64_t* bytes_read);
+  char* ReadMessage(int64_t* bytes_read);
 
-  // Indicates 'len' bytes are done being read. Not valid to be read again until the
-  // next call to Read()
-  void DoneRead(int64_t len);
+  // Indicates the previous message from ReadMessage() is no longer used. The memory
+  // is not valid anymore.
+  void ReadDone();
 
   const std::string& path() const { return path_; }
   int fd() const { return fd_; }
 
  private:
+  // The first part of the mmapped region is used as a control structure. It contains
+  // the Control structure followed by max_messages * sizeof(Message).
+  struct Message {
+    int64_t len;
+    int64_t offset;
+  };
+
+  struct Control {
+    // Maximum number of messages that can be queued.
+    int max_messages;
+
+    // Logical indices for the next write and read messages. The reader only updates
+    // write_idx and the writter only updates write index.
+    // write_idx >= read_idx and write_idx == read_idx indicates the stream is empty.
+    uint64_t write_idx;
+    uint64_t read_idx;
+
+    Message messages[0];
+  };
+
   SharedMemoryStream() : fd_(-1), memory_(NULL) {}
 
   std::string path_;
   int64_t capacity_;
+  int max_messages_;
   int fd_;
   bool is_writer_;
 
+  Control* control_;
+
+  // First byte that is usable for client data.
   char* memory_;
+
+  // Writer only state
+  char* write_head_;
 };
 
 #endif
