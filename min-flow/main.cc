bae50fed4782c59c7dcec808f59c5424320d280f
Nong Li <nongli@gmail.com>
2018-09-07 00:22:01 -0700

    Generalize kauai

--- min-flow/main.cc
+++ min-flow/main.cc
@@ -4,39 +4,27 @@
 #include <vector>
 using namespace std;
 
+const char* PEOPLE[] = {
+  "Marshall",
+  "Ariana",
+  "Itay",
+  "Nong",
+  "Panda",
+  "Mike",
+  "Nisha",
+  "Skye",
+};
+
+const int NUM_PEOPLE = sizeof(PEOPLE) / sizeof(PEOPLE[0]);
+
 void printCsv(float* g) {
-  for (int i = 0; i < 8; i++) {
-    switch (i) {
-      case 0:
-        printf("Marshall");
-        break;
-      case 1:
-        printf("Ariana");
-        break;
-      case 2:
-        printf("Itay");
-        break;
-      case 3:
-        printf("Nong");
-        break;
-      case 4:
-        printf("Panda");
-        break;
-      case 5:
-        printf("Mike");
-        break;
-      case 6:
-        printf("Nisha");
-        break;
-      case 7:
-        printf("Skye");
-        break;
-    }
-    for (int j = 0; j < 8; j++) {
-      if (g[i * 8 + j] == 0) {
+  for (int i = 0; i < NUM_PEOPLE; i++) {
+    printf("%-10s", PEOPLE[i]);
+    for (int j = 0; j < NUM_PEOPLE; j++) {
+      if (g[i * NUM_PEOPLE + j] == 0) {
         printf("\t/");
       } else {
-        printf("\t%0.2f", g[i * 8 + j]);
+        printf("\t%0.2f", g[i * NUM_PEOPLE + j]);
       }
     }
     printf("\n");
@@ -53,17 +41,22 @@ void printGraph(float* g, int w) {
 }
 
 bool simplify(float* g, int w, int srcRow, int srcCol, int dstRow, int dstCol) {
-  //printf("Trying: %d,%d %d,%d\n", srcRow, srcCol, dstRow, dstCol);
-  float v1 = g[srcCol * 8 + dstCol];
-  float v2 = g[dstCol * 8 + srcCol];
+  float v1 = g[srcCol * NUM_PEOPLE + dstCol];
+  float v2 = g[dstCol * NUM_PEOPLE + srcCol];
   if (v1 != 0) {
-    printf("1 %f\n", v1);
+    if (g[srcRow * NUM_PEOPLE + srcCol] < v1 && g[srcRow * NUM_PEOPLE + dstCol] != 0) {
+      printGraph(g, w);
+      printf("\n");
+      g[srcCol * NUM_PEOPLE + dstCol] -= g[srcRow * NUM_PEOPLE + srcCol];
+      g[srcRow * NUM_PEOPLE + dstCol] += g[srcRow * NUM_PEOPLE + srcCol];
+      g[srcRow * NUM_PEOPLE + srcCol] = 0;
+      return true;
+    }
   } else if (v2 != 0) {
-    if (g[dstRow * 8 + dstCol] < v2) {
-      printf("Removing %d %d\n", dstRow, dstCol);
-      g[dstCol * 8 + srcCol] -= g[dstRow * 8 + dstCol];
-      g[srcRow * 8 + srcCol] += g[dstRow * 8 + dstCol];
-      g[dstRow * 8 + dstCol] = 0;
+    if (g[dstRow * NUM_PEOPLE + dstCol] < v2 && g[srcRow * NUM_PEOPLE + srcCol] != 0) {
+      g[dstCol * NUM_PEOPLE + srcCol] -= g[dstRow * NUM_PEOPLE + dstCol];
+      g[srcRow * NUM_PEOPLE + srcCol] += g[dstRow * NUM_PEOPLE + dstCol];
+      g[dstRow * NUM_PEOPLE + dstCol] = 0;
       return true;
     }
   }
@@ -91,44 +84,47 @@ bool simplify(float* g, int w) {
 }
 
 int main(int argc, char** argv) {
-  float* graph = new float[8 * 8];
-  memset(graph, 0, sizeof(float) * 8 * 8);
-  graph[0 * 8 + 2] = 381.81;
-  graph[0 * 8 + 4] = 83.46;
+  float* graph = new float[NUM_PEOPLE * NUM_PEOPLE];
+  memset(graph, 0, sizeof(float) * NUM_PEOPLE * NUM_PEOPLE);
+  graph[0 * NUM_PEOPLE + 2] = 381.81;
+  graph[0 * NUM_PEOPLE + 4] = 83.46;
 
-  graph[1 * 8 + 0] = 231.75;
-  graph[1 * 8 + 2] = 467.09;
-  graph[1 * 8 + 3] = 30.29;
-  graph[1 * 8 + 4] = 315.21;
-  graph[1 * 8 + 5] = 6.61;
-  graph[1 * 8 + 7] = 36.26;
+  graph[1 * NUM_PEOPLE + 0] = 231.75;
+  graph[1 * NUM_PEOPLE + 2] = 467.09;
+  graph[1 * NUM_PEOPLE + 3] = 30.29;
+  graph[1 * NUM_PEOPLE + 4] = 315.21;
+  graph[1 * NUM_PEOPLE + 5] = 6.61;
+  graph[1 * NUM_PEOPLE + 7] = 36.26;
 
-  graph[3 * 8 + 0] = 198.96;
-  graph[3 * 8 + 2] = 467.49;
-  graph[3 * 8 + 4] = 282.43;
+  graph[3 * NUM_PEOPLE + 0] = 198.96;
+  graph[3 * NUM_PEOPLE + 2] = 467.49;
+  graph[3 * NUM_PEOPLE + 4] = 282.43;
 
-  graph[4 * 8 + 2] = 0;
+  graph[4 * NUM_PEOPLE + 2] = 0;
 
-  graph[5 * 8 + 0] = 235.47;
-  graph[5 * 8 + 2] = 460.49;
-  graph[5 * 8 + 3] = 23.68;
-  graph[5 * 8 + 4] = 283.08;
-  graph[5 * 8 + 7] = 23.43;
+  graph[5 * NUM_PEOPLE + 0] = 235.47;
+  graph[5 * NUM_PEOPLE + 2] = 460.49;
+  graph[5 * NUM_PEOPLE + 3] = 23.68;
+  graph[5 * NUM_PEOPLE + 4] = 283.08;
+  graph[5 * NUM_PEOPLE + 7] = 23.43;
 
-  graph[6 * 8 + 0] = 75.61;
-  graph[6 * 8 + 2] = 467.09;
-  graph[6 * 8 + 3] = 30.29;
-  graph[6 * 8 + 4] = 203.92;
-  graph[6 * 8 + 5] = 6.61;
-  graph[6 * 8 + 7] = 30.04;
+  graph[6 * NUM_PEOPLE + 0] = 75.61;
+  graph[6 * NUM_PEOPLE + 2] = 467.09;
+  graph[6 * NUM_PEOPLE + 3] = 30.29;
+  graph[6 * NUM_PEOPLE + 4] = 203.92;
+  graph[6 * NUM_PEOPLE + 5] = 6.61;
+  graph[6 * NUM_PEOPLE + 7] = 30.04;
 
-  graph[7 * 8 + 0] = 207.08;
-  graph[7 * 8 + 2] = 422.43;
-  graph[7 * 8 + 3] = 8.12;
-  graph[7 * 8 + 4] = 271.38;
+  graph[7 * NUM_PEOPLE + 0] = 207.08;
+  graph[7 * NUM_PEOPLE + 2] = 422.43;
+  graph[7 * NUM_PEOPLE + 3] = 8.12;
+  graph[7 * NUM_PEOPLE + 4] = 271.38;
 
   printCsv(graph);
-  simplify(graph, 8);
-  printGraph(graph, 8);
+  printf("\n");
+  simplify(graph, NUM_PEOPLE);
+  printGraph(graph, NUM_PEOPLE);
+  printf("\n");
+  simplify(graph, NUM_PEOPLE);
   printCsv(graph);
 }
